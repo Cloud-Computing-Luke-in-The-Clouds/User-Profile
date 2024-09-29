8199407b9824fc30839d9c465e75e809442282a6
schume wang <schumewang@wangshengli-Mac.lan>
2024-09-29 15:08:58 -0400

    change to use

--- app/models/researcher.py
+++ app/models/researcher.py
@@ -5,39 +5,35 @@ from typing import Optional
 from pydantic import BaseModel
 
 
-class ResearchPaper(BaseModel):
-    paper_title: Optional[str] = None
-    paper_link: Optional[str] = None
-    demo_video_link: Optional[str] = None
-    project_website: Optional[str] = None
+class Interest_Field(BaseModel):
+    interest_name: Optional[str] = None
 
 
-class ResearchProfile(BaseModel):
-    google_scholar_link: Optional[str] = None
-    personal_website_link: Optional[str] = None
-    organization: Optional[str] = None
-    title: Optional[str] = None
+class UserProfile(BaseModel):
+    user_id: Optional[str] = None
+    email_address: Optional[str] = None
+    name: Optional[str] = None
+    password: Optional[str] = None
     age: Optional[int] = None
     sex: Optional[str] = None
-    paper: Optional[list[ResearchPaper]] = None
+    interest_list: Optional[list[Interest_Field]] = None
+    points: Optional[int]
 
 
 class Config:
     json_schema_extra = {
         "example": {
-            "google_scholar_link": "https://scholar.google.com/citations?user=l2g4PFYAAAAJ&hl=en",
-            "personal_website_link": "https://www.chingyitsai.com/",
-            "organization": "Princeton University",
-            "title": "Ph.D. Student",
-            "age": 27,
+            "user_id": "123",
+            "email_address": "testuser@columbia.edu",
+            "name": "John Doe",
+            "password": "XXXXXXXX",
+            "age": 18,
             "sex": "Male",
-            "paper": [
+            "interest_list": [
                 {
-                    "paper_title": "Airracket: Perceptual design of ungrounded, directional force feedback to improve virtual racket sports experiences",
-                    "paper_link": "https://dl.acm.org/doi/abs/10.1145/3491102.3502034", 
-                    "demo_video_link": "https://www.youtube.com/watch?v=UAoQlSbQRYY",
-                    "project_website": "https://www.chingyitsai.com/_posts/projects/AirRacket.html", 
+                    "interest_name": "Cloud Computing"
                 }
-            ]
+            ],
+            "points": 2
         }
     }
--- app/routers/researcher.py
+++ app/routers/researcher.py
@@ -1,17 +1,17 @@
 from fastapi import APIRouter
 
-from app.models.researcher import ResearchProfile
-from app.resources.researcher_resource import ResearcherResource
+from app.models.researcher import UserProfile
+from app.resources.researcher_resource import UserResource
 from app.services.service_factory import ServiceFactory
 
 router = APIRouter()
 
 
-@router.get("/researcher_profile/{researcher_id}", tags=["users"])
-async def get_researcher(researcher_id: str) -> ResearchProfile:
+@router.get("/user_profile/{user_id}", tags=["users"])
+async def get_researcher(user_id: str) -> UserProfile:
 
     # TODO Do lifecycle management for singleton resource
-    res = ServiceFactory.get_service("ResearcherResource")
-    result = res.get_by_key(researcher_id)
+    res = ServiceFactory.get_service("UserResource")
+    result = res.get_by_key(user_id)
     return result
 
